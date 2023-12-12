2e0c3fd4d885cbf41c985e96f880e3cf65df2ff7
Rick South <rick@natamus.com>
2023-12-12 10:26:25 +0100

    Easy Elytra Takeoff source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/easyelytratakeoff/neoforge/events/NeoForgeElytraEvent.java
@@ -0,0 +1,30 @@
+package com.natamus.easyelytratakeoff.neoforge.events;
+
+import com.natamus.easyelytratakeoff.events.ElytraEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeElytraEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || e.phase.equals(TickEvent.Phase.START)) {
+			return;
+		}
+
+		ElytraEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onFirework(PlayerInteractEvent.RightClickItem e) {
+		ElytraEvent.onFirework(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
