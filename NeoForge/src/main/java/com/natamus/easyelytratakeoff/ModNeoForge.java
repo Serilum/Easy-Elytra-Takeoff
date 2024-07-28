d524ab73ff00776cf2165e4c030e8d08eec354b2
Rick South <rick@natamus.com>
2024-07-28 07:35:48 +0200

    (v4.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/easyelytratakeoff/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/easyelytratakeoff/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.easyelytratakeoff;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.easyelytratakeoff.neoforge.events.NeoForgeElytraEvent;
 import com.natamus.easyelytratakeoff.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
