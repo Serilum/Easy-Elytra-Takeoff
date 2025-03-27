745c8e01e6601a9a890048cbb3aebbb31d49bc1d
Rick South <rick@natamus.com>
2025-03-28 00:53:03 +0100

    Easy Elytra Takeoff source code for 1.21.5.

---
+++ Common/src/main/java/com/natamus/easyelytratakeoff/services/helpers/ElytraEventHelper.java
@@ -0,0 +1,7 @@
+package com.natamus.easyelytratakeoff.services.helpers;
+
+import net.minecraft.world.entity.player.Player;
+
+public interface ElytraEventHelper {
+    boolean isWearingAnElytra(Player player);
+}
---
+++ Forge/src/main/java/com/natamus/easyelytratakeoff/forge/events/ForgeElytraEvent.java
@@ -0,0 +1,28 @@
+package com.natamus.easyelytratakeoff.forge.events;
+
+import com.natamus.easyelytratakeoff.events.ElytraEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeElytraEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || e.phase.equals(TickEvent.Phase.START)) {
+			return;
+		}
+
+		ElytraEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onFirework(PlayerInteractEvent.RightClickItem e) {
+		ElytraEvent.onFirework(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
