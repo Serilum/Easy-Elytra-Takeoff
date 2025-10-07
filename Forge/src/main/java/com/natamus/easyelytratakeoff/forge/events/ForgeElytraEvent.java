e16f65b4871cce97aa4118ebc044e39cedf60f5f
Rick South <rick@natamus.com>
2025-10-08 01:40:52 +0200

    Easy Elytra Takeoff source code for 1.21.10.

---
+++ Forge/src/main/java/com/natamus/easyelytratakeoff/forge/events/ForgeElytraEvent.java
@@ -0,0 +1,35 @@
+package com.natamus.easyelytratakeoff.forge.events;
+
+import com.natamus.easyelytratakeoff.events.ElytraEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeElytraEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeElytraEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent.Pre e) {
+		Player player = e.player();
+		Level level = player.level();
+		if (level.isClientSide()) {
+			return;
+		}
+
+		ElytraEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onFirework(PlayerInteractEvent.RightClickItem e) {
+		ElytraEvent.onFirework(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/easyelytratakeoff/neoforge/services/NeoForgeElytraEventHelper.java
@@ -0,0 +1,51 @@
+package com.natamus.easyelytratakeoff.neoforge.services;
+
+import com.natamus.collective.data.Constants;
+import com.natamus.easyelytratakeoff.services.helpers.ElytraEventHelper;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Collection;
+
+public class NeoForgeElytraEventHelper implements ElytraEventHelper {
+	@Override
+	public boolean isWearingAnElytra(Player player) {
+        boolean foundelytra = false;
+		for (EquipmentSlot equipmentSlot : Constants.equipmentSlots) {
+			if (equipmentSlot.equals(EquipmentSlot.OFFHAND)) {
+				continue;
+			}
+
+			ItemStack slotStack = player.getItemBySlot(equipmentSlot);
+			if (!slotStack.isEmpty()) {
+				if (slotStack.has(DataComponents.GLIDER)) {
+					foundelytra = true;
+					break;
+				}
+			}
+		}
+
+		if (!foundelytra) {
+			Collection<AttributeInstance> atrb = player.getAttributes().getSyncableAttributes();
+			for (AttributeInstance ai : atrb) {
+				for (AttributeModifier m : ai.getModifiers()) {
+					String name = m.toString().toLowerCase();
+					if (name.contains("flight modifier") || name.contains("elytra curio modifier")) {
+						if (m.amount() >= 1.0) {
+							foundelytra = true;
+							break;
+						}
+					}
+				}
+				if (foundelytra) {
+					break;
+				}
+			}
+		}
+		return foundelytra;
+	}
+}
