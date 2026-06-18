e345c9d3018a5e110bf18199055c8a0d12c3578f
Rick South <rick@natamus.com>
2026-06-18 23:58:47 +0200

    Updated Easy Elytra Takeoff to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/easyelytratakeoff/util/Reference.java
+++ Common/src/main/java/com/natamus/easyelytratakeoff/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "easyelytratakeoff";
 	public static final String NAME = "Easy Elytra Takeoff";
 	public static final String VERSION = "4.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
